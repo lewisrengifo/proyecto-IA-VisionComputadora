266387dbf24e9c22b46771a9236d63f2b2b0d2cc
lewisr <l.rengifo@pucp.edu.pe>
2022-03-19 20:55:17 -0500

    train w cifar10 and real_predictor

--- proyecto_grupal.py
+++ proyecto_grupal.py
@@ -1,8 +1,17 @@
+from __future__ import print_function
 from statistics import median
 import cv2
 from matplotlib import pyplot as plt
 import os
 import numpy as np
+import keras
+print(f"keras{keras.__version__}")
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from tensorflow.keras.utils import to_categorical
 
 #input data
 #auto
@@ -29,23 +38,93 @@ dim = (width, height)
 img2Resized = cv2.resize(img2,dim, interpolation= cv2.INTER_AREA)
 print(f'shape: {img2Resized.shape}')
 #resize show
-cv2.imshow('auto', img1Resized)
-cv2.imshow('moto', img2Resized)
-cv2.waitKey(0)
+#cv2.imshow('auto', img1Resized)
+#cv2.imshow('moto', img2Resized)
 #construcion de un histograma para tener criterios de seleccion de filtros
 plt.hist(img1Resized.ravel(),256,[0,256])
 plt.hist(img2Resized.ravel(),256,[0,256])
-plt.show()
+#plt.show()
 
 #filtros
 image_tofilter = img1Resized
 normal_blur = cv2.blur(image_tofilter, (5,5))
 median_blur = cv2.medianBlur(image_tofilter, 5)
 gaussian_blur = cv2.GaussianBlur(image_tofilter,(5,5),0)
-cv2.imshow('normal blur',normal_blur)
-cv2.imshow('median blur',median_blur)
-cv2.imshow('gaussian blur',gaussian_blur)
-cv2.waitKey(0)
+#cv2.imshow('normal blur',normal_blur)
+#cv2.imshow('median blur',median_blur)
+#cv2.imshow('gaussian blur',gaussian_blur)
 #edge detection(?
 #show
+##train tests
+'''Trains a simple convnet on the MNIST dataset.
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+
+
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+##################################################
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+#print(f"label xtrain [0] {x_train[0]}")
+
+print(f'image data format: {K.image_data_format()}')
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
+    input_shape = (3, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
+
+
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')#0-255
+x_train /= 255
+x_test /= 255 # 0-1.0
+#print('x_train shape:', x_train.shape)
+#print(x_train.shape[0], 'train samples')
+#print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+print(f'xtrain: {x_train[0].shape}')
+y_train = to_categorical(y_train, num_classes)#[0,0,0,0,0,0,0,0,0,0,1] onehot encoding..
+y_test = to_categorical(y_test, num_classes)
+###################################################
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))#[0.5,0.2,0.1,0.1,0.0,0.0..]
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer='adam',
+              metrics=['accuracy'])
+
+model.summary()
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#Guardar el Modelo
+model.save('final_model.h5')
 
--- real_predictor.py
+++ real_predictor.py
@@ -18,17 +18,25 @@ import pandas as pd
 
 
 Model_Name= "my_model"
-model_save_path = "model path"
+model_save_path = "path_to_my_model.h5"
 model = load_model(model_save_path)
 
-img_np = cv2.imread("image_path.jpg")  # cv2.IMREAD_COLOR in OpenCV 3.1
-img_RGB = cv2.cvtColor(img_np, cv2.COLOR_GRAY2RGB)
+
+img_np = cv2.imread("cafe.jpg")  # cv2.IMREAD_COLOR in OpenCV 3.1
+
+dim = (32, 32)
+resized= cv2.resize(img_np, dim)
+print(f'shape: {img_np.shape}')
+img_to_process = np.expand_dims(resized, axis=0)
+
+#img_RGB = cv2.cvtColor(img_np, cv2.COLOR_GRAY2RGB)
 # cv2.imshow("imgs",img_RGB)
 # cv2.waitKey(0)
-img_ar = np.asarray(img_RGB)
-img_ar = img_ar / 255
+#img_ar = np.asarray(img_np)
+#img_ar = img_ar / 255
 # print(f"shape : {img_ar.shape}")
-model_input = np.expand_dims(img_ar, axis=0)
-pred = model.predict(model_input)
-
+#model_input = np.expand_dims(img_ar, axis=0)
+pred = model.predict(img_to_process)
+print(f'pred: {pred}')
 top = np.argmax(pred)
+print(f'top: {top}')
